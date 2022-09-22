da1393679240acbdaaf2d7c0a35f6a30bd433ebc
Alexsandro VcT <alexsandro.vct@outlook.com>
2022-09-22 00:01:32 -0300

    conexao-Banco-De-Dados

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
-define('HOST', '127.0.0.1');
-define('USUARIO', 'root');
-define('SENHA', 'ecommerce');
-define('DB', 'login');
+define('HOST', 'localhost');
+define('USUARIO', 'id19605917_ecommerce');
+define('SENHA', 'A^H9VLshhRzR&e0^');
+define('DB', 'id19605917_login');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
